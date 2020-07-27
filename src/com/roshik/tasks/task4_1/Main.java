405028c2ae2aee3072d2fe904c082971e1fa8121
roshik8 <roshik8@gmail.com>
2020-07-27 13:44:09 +0500

    замена массива на ArrayList в списке объектов машин

--- src/com/roshik/tasks/task4_1/Main.java
+++ src/com/roshik/tasks/task4_1/Main.java
@@ -1,14 +1,15 @@
 package com.roshik.tasks.task4_1;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-public class Main{
-    enum carParameter{
-        gosNumber,distance,additional
+public class Main {
+
+    enum carParameter {
+        gosNumber, distance, additional
     }
 
     public static String[] ts = {"C100_1-100", "C200_1-120-1200", "C300_1-120-30", "C400_1-80-20", "C100_2-50", "C200_2-40-1000", "C300_2-120-45", "C400_2-10-20", "C100_3-10", "C200_3-170-1100", "C300_3-150-29", "C400_3-100-28", "C100_1-300", "C200_1-100-750", "C300_1-32-15"};
+
     public static BaseCar createCar(String str) {
         int indexOf = str.indexOf("_");
         String codeCar = str.substring(0, indexOf);
@@ -28,23 +29,11 @@ public class Main{
         }
     }
 
-    public static String getCarName(BaseCar[] cars, String carType)
-    {
-        for (BaseCar i : cars)
-        {
-            if (i.getClass().getSimpleName() == carType)
-                return i.carName;
-        }
-
-        return "";
-    }
-
-    private static HashMap<carParameter,Integer> parse(String parameterSting) {
-        HashMap<carParameter,Integer> map = new HashMap();
+    private static HashMap<carParameter, Integer> parse(String parameterSting) {
+        HashMap<carParameter, Integer> map = new HashMap<>();
 
         int start = 0;
-        for(carParameter i: carParameter.values())
-        {
+        for (carParameter i : carParameter.values()) {
             int end = parameterSting.indexOf("-", start);
             String substring;
             if (end < 0)
@@ -60,147 +49,87 @@ public class Main{
         return map;
     }
 
-    public static String[] getArrCarType(BaseCar[] cars)
-    {
-        int countDiffCarCode = 0;
-        String[] carCode = new String[cars.length];
+    public static HashMap<String, Double> getTypeSumCostGSM(ArrayList<BaseCar> cars) {
+        HashMap<String, Double> typeSumCostGSM = new HashMap<>();
 
-        for (int i = 0; i < cars.length; i++)
-        {
-            String carType = cars[i].getClass().getSimpleName();
-            boolean carCodeAlreadyAdd = false;
-
-            for (int j = 0; j < countDiffCarCode; j++)
-            {
-                if (carCode[j] == carType)
-                {
-                    carCodeAlreadyAdd = true;
-                    j = countDiffCarCode; //выход из цикла поиска дублей
-                }
-            }
-
-            if (!carCodeAlreadyAdd)
-            {
-                carCode[countDiffCarCode++] = carType;
-            }
+        for (BaseCar car : cars) {
+            double sumCost = typeSumCostGSM.getOrDefault(car.carName, 0.0);
+            sumCost += car.getSumCostGsm();
+            typeSumCostGSM.put(car.carName, sumCost);
         }
 
-        String[] carDiffCode = new String[countDiffCarCode];
-        for (int i = 0; i < carDiffCode.length; i++)
-        {
-            carDiffCode[i] = carCode[i];
-        }
-
-        return carDiffCode;
-    }
-    public static double getTypeSumCostGsm(BaseCar[] cars, String carType)
-    {
-        double sum = 0;
-        for (BaseCar i : cars)
-        {
-            if (i.getClass().getSimpleName() == carType)
-                sum += i.getSumCostGsm();
-        }
-        return sum;
+        return typeSumCostGSM;
     }
 
-    public static String getMaxCarCostSumGsm(BaseCar[] cars)
-    {
-        double maxGsm = 0;
-        String carCodeMax = "BaseCar";
-        for (String i : getArrCarType(cars))
-        {
-            double currentSumCostGsm = getTypeSumCostGsm(cars, i);
-            if (maxGsm <= currentSumCostGsm)
-            {
+    public static String getMaxCarCostSumGsm(HashMap<String, Double> typeSumCostGSM) {
+        Double maxGsm = null;
+        String carName = "";
+        for (var i : typeSumCostGSM.entrySet()) {
+            double currentSumCostGsm = i.getValue();
+            if (maxGsm == null || maxGsm <= currentSumCostGsm) {
                 maxGsm = currentSumCostGsm;
-                carCodeMax = i;
+                carName = i.getKey();
             }
         }
 
-        return getCarName(cars, carCodeMax);
+        return carName;
     }
 
-    public static String getMinCarCostSumGsm(BaseCar[] cars)
-    {
-        double minGsm = getTypeSumCostGsm(cars, cars[0].getClass().getSimpleName());
-        String carCodeMin = cars[0].getClass().getSimpleName();
-        for (String i : getArrCarType(cars))
-        {
-            double currentSumCostGsm = getTypeSumCostGsm(cars, i);
-            if (minGsm > currentSumCostGsm)
-            {
+    public static String getMinCarCostSumGsm(HashMap<String, Double> typeSumCostGSM) {
+        Double minGsm = null;
+        String carName = "";
+        for (var i : typeSumCostGSM.entrySet()) {
+            double currentSumCostGsm = i.getValue();
+            if (minGsm == null || minGsm > currentSumCostGsm) {
                 minGsm = currentSumCostGsm;
-                carCodeMin = i;
+                carName = i.getKey();
             }
         }
 
-        return getCarName(cars, carCodeMin);
+        return carName;
     }
 
-    public static BaseCar[] getCarTypeArr(BaseCar[] cars, String carType) {
-        int countCarType = 0;
-        for (BaseCar i : cars) {
-            countCarType += i.getClass().getSimpleName() == carType ? 1 : 0;
+    static Comparator<BaseCar> nameComparator = new Comparator<>() {
+        @Override
+        public int compare(BaseCar o1, BaseCar o2) {
+            return o1.carName.compareTo(o2.carName);
         }
-        BaseCar[] CarArray = new BaseCar[countCarType];
-
-        for (int i = 0, j = 0; i < cars.length; i++) {
-            if (cars[i].getClass().getSimpleName() == carType) {
-                CarArray[j++] = cars[i];
-            }
+    };
+    static Comparator<BaseCar> distanceComparator = new Comparator<>() {
+        @Override
+        public int compare(BaseCar o1, BaseCar o2) {
+            if (o1.distance < o2.distance) return -1;
+            if (o1.distance > o2.distance) return 1;
+            return 0;
         }
+    };
 
-        return CarArray;
-
+    private static void sortCarList(ArrayList<BaseCar> cars) {
+        Collections.sort(cars, nameComparator.thenComparing(distanceComparator));
     }
-
-    public static BaseCar[] sortCarArray(BaseCar[] cars) {
-        for (int i = cars.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (cars[j].distance > cars[j + 1].distance){
-                    BaseCar tmp = cars[j];
-                    cars[j] = cars[j + 1];
-                    cars[j + 1] = tmp;
-                }
-            }
-        }
-        return cars;
-    }
-
 
     public static void main(String[] args) {
-
-        BaseCar[] cars = new BaseCar[ts.length];
-        for (int i = 0; i < ts.length; i++) {
-            cars[i] = createCar(ts[i]);
-        }
-        double allCostGsm =0;
-        for(BaseCar i:cars){
-
-            allCostGsm+=i.getSumCostGsm();
-
+        ArrayList<BaseCar> cars = new ArrayList<>();
+        for (String str : ts) {
+            cars.add(createCar(str));
         }
 
+        double allCostGsm = 0;
+        for (BaseCar car : cars) {
+            allCostGsm += car.getSumCostGsm();
+        }
         System.out.println("Расходы на ГСМ всего " + allCostGsm);
-        for (String i: getArrCarType(cars)){
-            System.out.println("Расходы на "+getCarName(cars,i)+": "+getTypeSumCostGsm(cars,i));
+
+        for (var carType : getTypeSumCostGSM(cars).entrySet()) {
+            System.out.println("Расходы на " + carType.getKey() + ": " + carType.getValue());
         }
-        System.out.println("Самые высокие расходы на " + getMaxCarCostSumGsm(cars));
-        System.out.println("Самые низкие расходы на " + getMinCarCostSumGsm(cars));
+        System.out.println("Самые высокие расходы на " + getMaxCarCostSumGsm(getTypeSumCostGSM(cars)));
+        System.out.println("Самые низкие расходы на " + getMinCarCostSumGsm(getTypeSumCostGSM(cars)));
 
-        for (String i : getArrCarType(cars)) {
-            for (BaseCar j : sortCarArray(getCarTypeArr(cars,i))) {
-                System.out.println(j.getCarInfo());
-            }
+        sortCarList(cars);
+        for (BaseCar car : cars) {
+            System.out.println(car.getCarInfo());
         }
-
-
-
-
-
     }
-
-
 }
 
